ce75267243a26fd45d9736c5c90fa5f7af80f389
dariogemo <dario.gemo@studenti.univr.it>
2025-09-16 13:42:32 +0200

    change poi for experiment to arena di verona. need to rerun all the experiments

--- scripts/data_analysis_giulietta.py
+++ scripts/data_analysis_giulietta.py
@@ -1,6 +1,7 @@
 import io
 import sys
 
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
@@ -12,11 +13,16 @@ run_figs: str = str(input("Do you want to display the figures? [{y}es, {n}o]\n")
 if run_figs in ["y", "yes"]:
     DPI = 0
     while DPI > 500 or DPI < 100:
-        DPI = int(
-            input("DPI for saving the figures. Enter a number between 100 and 500\n")
-        )
+        try:
+            DPI = int(
+                input(
+                    "DPI for saving the figures. Enter a number between 100 and 500\n"
+                )
+            )
+        except ValueError as e:
+            print("Please insert a valid number")
         if DPI > 500 or DPI < 100:
-            print(f'You\'ve entered "{DPI}", please enter a value between 100 and 500')
+            print("Please enter a value between 100 and 500")
 else:
     DPI = None
 
@@ -27,25 +33,28 @@ redir_stdout: str = str(
     )
 )
 if redir_stdout in ["y", "yes"]:
-    sys.stdout = open("txt_files/data_casa_di_giulietta_output.txt", "w")
+    sys.stdout = open(
+        "txt_files/data_casa_di_giulietta_output.txt", "w", encoding="utf-8"
+    )
 else:
     pass
 
 df: pd.DataFrame = pd.read_csv(
-    "../../main_dataset/count_data/data_casa_di_giulietta.csv",
+    "../../main_dataset/count_data/data_anfiteatro_arena.csv",
     index_col="date",
     parse_dates=["date"],
 )
 df: pd.DataFrame = df.drop("Unnamed: 0", axis=1)
+df.fillna(0, inplace=True)
 
 print(
     "Data's first 5 rows\n", df.head(), "\n--------------------------------------------"
 )
 
 print("Data info\n", df.describe(), "\n")
-buffer = io.StringIO()
+buffer: io.StringIO = io.StringIO()
 df.info(buf=buffer)
-info_str = buffer.getvalue()
+info_str: str = buffer.getvalue()
 print(info_str, "\n--------------------------------------------")
 
 print(
@@ -53,49 +62,80 @@ print(
     "\n--------------------------------------------",
 )
 
-if run_figs == "y" or run_figs == "yes":
-    fig, ax = plt.subplots(2, 2, figsize=(20, 20))
-    plt.suptitle("Giulietta's house variables", fontsize=40)
+if run_figs in ["y", "yes"]:
+    fig, ax = plt.subplots(2, 3, figsize=(20, 10))
+    plt.suptitle("Arena di Verona variables", fontsize=40)
     sns.lineplot(y=df["count"], x=df.index, linewidth=0.4, color="#1d4863", ax=ax[0][0])
     sns.lineplot(y=df["temp"], x=df.index, linewidth=0.3, color="#e8ad3a", ax=ax[0][1])
-    sns.lineplot(y=df["rain"], x=df.index, linewidth=0.3, color="#ca3ae8", ax=ax[1][0])
+    sns.lineplot(y=df["rain"], x=df.index, linewidth=0.3, color="#ca3ae8", ax=ax[0][2])
     sns.lineplot(
-        y=df["popularity"], x=df.index, linewidth=0.3, color="#5cad52", ax=ax[1][1]
+        y=df["festivity"], x=df.index, linewidth=0.3, color="#2596be", ax=ax[1][0]
     )
-    plt.savefig("img/data_analysis/giulietta_timeseries.png", dpi=DPI)
+    sns.lineplot(
+        y=df["rolling_mean_30d"],
+        x=df.index,
+        linewidth=0.3,
+        color="#78BE25",
+        ax=ax[1][1],
+    )
+    sns.lineplot(
+        y=df["above_avg"], x=df.index, linewidth=0.01, color="#BE8825", ax=ax[1][2]
+    )
+    for ax1 in ax.flatten():
+        ax1.set_xlabel(None)
+    plt.savefig("img/data_analysis/arena_timeseries.png", dpi=DPI)
+    plt.show()
 else:
     pass
 
 week_mask = (df.index > "2018-05-21") & (df.index <= "2018-05-28")
-week_df: pd.DataFrame = df[week_mask]
+week_df: pd.DataFrame = df.loc[week_mask]
 
-if run_figs == "y" and run_figs == "yes":
-    fig, ax = plt.subplots(2, 2, figsize=(20, 20))
-    plt.suptitle("Giulietta's house variables, week of 2018-05-28", fontsize=40)
+if run_figs in ["y", "yes"]:
+    fig, ax = plt.subplots(2, 3, figsize=(20, 10))
+    plt.suptitle("Arena di Verona variables, week of 2018-05-28", fontsize=40)
     sns.lineplot(
-        y=week_df["count"], x=week_df.index, linewidth=1.5, color="#1d4863", ax=ax[0][0]
+        y=week_df["count"], x=week_df.index, linewidth=1, color="#1d4863", ax=ax[0][0]
     )
     sns.lineplot(
-        y=week_df["temp"], x=week_df.index, linewidth=3, color="#e8ad3a", ax=ax[0][1]
-    )
-    ax[0][1].set_ylim(0, 4.3)
-    sns.lineplot(
-        y=week_df["rain"], x=week_df.index, linewidth=3, color="#ca3ae8", ax=ax[1][0]
+        y=week_df["temp"], x=week_df.index, linewidth=1.5, color="#e8ad3a", ax=ax[0][1]
     )
     sns.lineplot(
-        y=week_df["popularity"],
+        y=week_df["rain"], x=week_df.index, linewidth=1.5, color="#ca3ae8", ax=ax[0][2]
+    )
+    sns.lineplot(
+        y=week_df["festivity"],
         x=week_df.index,
-        linewidth=3,
-        color="#5cad52",
+        linewidth=1.5,
+        color="#2596be",
+        ax=ax[1][0],
+    )
+    sns.lineplot(
+        y=week_df["rolling_mean_30d"],
+        x=week_df.index,
+        linewidth=1.5,
+        color="#78BE25",
         ax=ax[1][1],
     )
-    ax[1][1].set_ylim(0, 19.3)
-    plt.savefig("img/data_analysis/giulietta_timeseries_oneweek.png", dpi=DPI)
+    sns.lineplot(
+        y=week_df["above_avg"],
+        x=week_df.index,
+        linewidth=0.5,
+        color="#BE8825",
+        ax=ax[1][2],
+    )
+    for ax1 in ax.flatten():
+        ax1.xaxis.set_major_locator(mdates.DayLocator())
+        ax1.xaxis.set_major_formatter(mdates.DateFormatter("%b %d"))
+        ax1.tick_params(axis="x", rotation=45)
+        ax1.set_xlabel(None)
+    plt.savefig("img/data_analysis/arena_timeseries_oneweek.png", dpi=DPI)
+    plt.show()
 else:
     pass
 
-max_count_mask = df["count"] == df["count"].max()
-max_count: pd.DataFrame = df[max_count_mask]
+max_count_mask: pd.Series = df["count"] == df["count"].max()
+max_count: pd.DataFrame = df.loc[max_count_mask]
 print(
     "The maximum number of tourists entering Giulietta's house was",
     int(max_count.iloc[0]["count"]),
@@ -105,5 +145,5 @@ print(
 
 print(max_count.index[0])
 
-if redir_stdout == "y" or redir_stdout == "yes":
+if redir_stdout in ["y", "yes"]:
     sys.stdout.close()
--- scripts/dataset_frequency.py
+++ scripts/dataset_frequency.py
@@ -1,16 +1,16 @@
 import os
-import sys
+from contextlib import redirect_stderr, redirect_stdout
 
 import pandas as pd
 
-sys.stdout = open("txt_files/dataset_frequency.txt", "w")
+with open("txt_files/dataset_frequency.txt", "w", encoding="utf-8") as txt_file:
+    with redirect_stdout(txt_file), redirect_stderr(txt_file):
+        repo_path: str = "./../../main_dataset/count_data/"
 
-repo_path = "./../../main_dataset/count_data/"
-
-for root, dirs, files in os.walk(repo_path):
-    for file in files:
-        file_path = os.path.join(root, file)
-        df : pd.DataFrame= pd.read_csv(file_path)
-        first_row = df.iloc[0]["date"]
-        last_row = df.iloc[-1]["date"]
-        print(file, first_row, last_row)
+        for root, dirs, files in os.walk(repo_path):
+            for file in files:
+                file_path: str = os.path.join(root, file)
+                df: pd.DataFrame = pd.read_csv(file_path)
+                first_row: str = df.iloc[0]["date"]
+                last_row: str = df.iloc[-1]["date"]
+                print(file, first_row, last_row)
