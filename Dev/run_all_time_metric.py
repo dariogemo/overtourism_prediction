530a1b4a402804c7298162982a117ab7d5b30045
dariogemo <dario.gemo@studenti.univr.it>
2026-01-21 12:42:56 +0100

    up to date

--- Dev/run_all_time_metric.py
+++ Dev/run_all_time_metric.py
@@ -167,13 +167,15 @@ if __name__ == "__main__":
         ),
         "DLinear",
     )
+    """
 
     main(get_abs_path("PatchTST/scripts/PatchTST/arena_2020_patchtst.sh"), "PatchTST")
 
+    """
     main(get_abs_path("Informer2020/scripts/arena_2020_informer.sh"), "Informer2020")
 
-    """
     main(
         get_abs_path("TimeMixer/scripts/long_term_forecast/arena_2020_timemixer.sh"),
         "TimeMixer",
     )
+    """
